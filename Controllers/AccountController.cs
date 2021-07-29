57af7cb312bf7b2a5f465e148fa7483c67b2bfb2
ifte99 <ifte9931@gmail.com>
2021-07-29 23:56:49 +0600

    Added Lecturers Management Module

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -25,7 +25,7 @@ namespace UiversityManagementSystem.Controllers
             context = new ApplicationDbContext();
         }
 
-        public AccountController(ApplicationUserManager userManager, ApplicationSignInManager signInManager )
+        public AccountController(ApplicationUserManager userManager, ApplicationSignInManager signInManager)
         {
             UserManager = userManager;
             SignInManager = signInManager;
@@ -37,9 +37,9 @@ namespace UiversityManagementSystem.Controllers
             {
                 return _signInManager ?? HttpContext.GetOwinContext().Get<ApplicationSignInManager>();
             }
-            private set 
-            { 
-                _signInManager = value; 
+            private set
+            {
+                _signInManager = value;
             }
         }
 
@@ -66,39 +66,39 @@ namespace UiversityManagementSystem.Controllers
 
         //
         // POST: /Account/Login
-        [HttpPost]
-        [AllowAnonymous]
-        [ValidateAntiForgeryToken]
-        public async Task<ActionResult> Login(LoginViewModel model, string returnUrl)
-        {
-            if (!ModelState.IsValid)
-            {
-                return View(model);
-            }
+     //   [HttpPost]
+     //   [AllowAnonymous]
+    //    [ValidateAntiForgeryToken]
+      //  public async Task<ActionResult> Login(LoginViewModel model, string returnUrl)
+     //   {
+       //     if (!ModelState.IsValid)
+        //    {
+        //        return View(model);
+        //    }
 
             // This doesn't count login failures towards account lockout
             // To enable password failures to trigger account lockout, change to shouldLockout: true
 
-            
-
-            var result = await SignInManager.PasswordSignInAsync(model.Username, model.Password, model.RememberMe, shouldLockout: false);
-            
 
 
-            switch (result)
-            {
-                case SignInStatus.Success:
-                    return RedirectToLocal(returnUrl);
-                case SignInStatus.LockedOut:
-                    return View("Lockout");
-                case SignInStatus.RequiresVerification:
-                    return RedirectToAction("SendCode", new { ReturnUrl = returnUrl, RememberMe = model.RememberMe });
-                case SignInStatus.Failure:
-                default:
-                    ModelState.AddModelError("", "Invalid login attempt.");
-                    return View(model);
-            }
-        }
+            // var result = await SignInManager.PasswordSignInAsync(model.Username, model.Password, model.RememberMe, shouldLockout: false);
+           // ApplicationUser signedUser = UserManager.FindByEmail(model.Email);
+           // var result = await SignInManager.PasswordSignInAsync(signedUser.UserName, model.Password, model.RememberMe, shouldLockout: false);
+
+          //  switch (result)
+          //  {
+             //   case SignInStatus.Success:
+             //       return RedirectToLocal(returnUrl);
+             //   case SignInStatus.LockedOut:
+            //        return View("Lockout");
+            //    case SignInStatus.RequiresVerification:
+            //        return RedirectToAction("SendCode", new { ReturnUrl = returnUrl, RememberMe = model.RememberMe });
+            //    case SignInStatus.Failure:
+           //     default:
+            //        ModelState.AddModelError("", "Invalid login attempt.");
+            //        return View(model);
+        //    }
+      //  }
 
         //
         // GET: /Account/VerifyCode
@@ -129,7 +129,7 @@ namespace UiversityManagementSystem.Controllers
             // If a user enters incorrect codes for a specified amount of time then the user account 
             // will be locked out for a specified amount of time. 
             // You can configure the account lockout settings in IdentityConfig
-            var result = await SignInManager.TwoFactorSignInAsync(model.Provider, model.Code, isPersistent:  model.RememberMe, rememberBrowser: model.RememberBrowser);
+            var result = await SignInManager.TwoFactorSignInAsync(model.Provider, model.Code, isPersistent: model.RememberMe, rememberBrowser: model.RememberBrowser);
             switch (result)
             {
                 case SignInStatus.Success:
@@ -148,7 +148,7 @@ namespace UiversityManagementSystem.Controllers
         [AllowAnonymous]
         public ActionResult Register()
         {
-            ViewBag.Roles = new SelectList(context.Roles.Where(q => !q.Name.Contains("Admin")).ToList(),"Name", "Name");
+            ViewBag.Roles = new SelectList(context.Roles.Where(q => !q.Name.Contains("Admin")).ToList(), "Name", "Name");
             return View();
         }
 
@@ -161,11 +161,11 @@ namespace UiversityManagementSystem.Controllers
         {
             if (ModelState.IsValid)
             {
-                var user = new ApplicationUser { UserName = model.Username, Email = model.Email, BirthDate= model.BirthDate};
+                var user = new ApplicationUser { UserName = model.Username, Email = model.Email, BirthDate = model.BirthDate };
                 var result = await UserManager.CreateAsync(user, model.Password);
                 if (result.Succeeded)
                 {
-                    await SignInManager.SignInAsync(user, isPersistent:false, rememberBrowser:false);
+                    await SignInManager.SignInAsync(user, isPersistent: false, rememberBrowser: false);
 
                     // For more information on how to enable account confirmation and password reset please visit https://go.microsoft.com/fwlink/?LinkID=320771
                     // Send an email with this link
