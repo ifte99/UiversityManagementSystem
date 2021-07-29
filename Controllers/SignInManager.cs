ec568d2d3ebe4f49251e15a357df5f3adbfc1630
USER <USER@HPHOMELAPTOP>
2021-07-29 13:28:37 +0600

    Add project files.

---
+++ Controllers/SignInManager.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace UiversityManagementSystem.Controllers
+{
+    internal class SignInManager
+    {
+        public SignInManager()
+        {
+        }
+
+        internal Task PasswordSignInAsync(object username, string password, bool rememberMe, bool shouldLockout)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
